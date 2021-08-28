e6fbd64d8c522d326ea11c7d88b94fa93a1593a1
IInfinity <i.inf@outlook.com>
2021-08-28 20:45:28 +0800

    feat(app): import css by package

--- apps/customer-service/src/main.tsx
+++ apps/customer-service/src/main.tsx
@@ -1,7 +1,9 @@
 import ClipboardJS from 'clipboard';
+import 'normalize.css/normalize.css';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'siimple/dist/siimple.min.css';
 import App from './app/app';
 
 new ClipboardJS('.siimple-list-item');
