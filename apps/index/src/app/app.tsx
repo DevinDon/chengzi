6509e0021d30a5c0599c7ec248cfcc615346ccfc
IInfinity <i.inf@outlook.com>
2021-08-29 20:08:32 +0800

    fix(apps/index): fix url

--- apps/index/src/app/app.tsx
+++ apps/index/src/app/app.tsx
@@ -29,13 +29,13 @@ export default () => <StyledApp>
 
   <StyledList>
     <CardComponent title="客服话术">
-      <a href="./customer-service/">
+      <a href="/customer-service/">
         <StyledImage src={customerServiceCover} alt="点击前往客服话术工具页面" title="点击前往客服话术工具页面" />
       </a>
     </CardComponent>
 
     <CardComponent title="订单格式化">
-      <a href="./order-formatter/">
+      <a href="/order-formatter/">
         <StyledImage src={orderFormatterCover} alt="点击前往订单格式化工具页面" title="点击前往订单格式化工具页面" />
       </a>
     </CardComponent>
