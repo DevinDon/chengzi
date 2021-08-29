35f0246aef71c929147dd02009c5e0d04b730c2c
IInfinity <i.inf@outlook.com>
2021-08-29 14:59:39 +0800

    feat(apps/of): add of imports

--- apps/order-formatter/src/main.tsx
+++ apps/order-formatter/src/main.tsx
@@ -1,7 +1,11 @@
+import ClipboardJS from 'clipboard';
+import 'normalize.css/normalize.css';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-
+import 'siimple/dist/siimple.min.css';
 import App from './app/app';
+
+new ClipboardJS('.copy-text');
 
 ReactDOM.render(
   <StrictMode>
