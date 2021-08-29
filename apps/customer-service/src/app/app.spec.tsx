e307ade8c52feaa93e8a11505996ec125a699c2a
IInfinity <i.inf@outlook.com>
2021-08-29 14:44:54 +0800

    test(apps/cs): update test

--- apps/customer-service/src/app/app.spec.tsx
+++ apps/customer-service/src/app/app.spec.tsx
@@ -1,25 +1,37 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app';
+import { CategoriesProvider, ItemsProvider } from './states';
 
 describe('App', () => {
+
   it('should render successfully', () => {
     const { baseElement } = render(
       <BrowserRouter>
-        <App />
+        <CategoriesProvider>
+          <ItemsProvider>
+            <App />,
+          </ItemsProvider>
+        </CategoriesProvider>
       </BrowserRouter>
     );
 
     expect(baseElement).toBeTruthy();
   });
 
-  it('should have a greeting as the title', () => {
+  it('should have title', () => {
     const { getByText } = render(
       <BrowserRouter>
-        <App />
+        <CategoriesProvider>
+          <ItemsProvider>
+            <App />,
+          </ItemsProvider>
+        </CategoriesProvider>
       </BrowserRouter>
     );
 
-    expect(getByText('Welcome to customer-service!')).toBeTruthy();
+    expect(getByText('客服话术')).toBeTruthy();
+    expect(getByText('小橙子专属')).toBeTruthy();
   });
+
 });
