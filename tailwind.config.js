9fc35c15d0a05f7ce5bd4ad7aeb73b7bcbe32871
IInfinity <i.inf@outlook.com>
2021-09-02 18:06:19 +0800

    perf(npm): add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './apps/**/*/src/**/*.tsx',
+    './libs/**/*/src/**/*.tsx',
+  ],
+  darkMode: 'media', // false or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
