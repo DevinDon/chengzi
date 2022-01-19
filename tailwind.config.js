5c771a7b8e5dd6aea32f8a9bd6616f0b7c2153d6
IInfinity <i.inf@outlook.com>
2022-01-20 01:45:19 +0800

    chore(tailwindcss): update tailwind css 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  purge: [
-    './apps/**/*.tsx',
-    './libs/**/*.tsx',
+  content: [
+    'apps/**/*/src/**/*.{js,jsx,ts,tsx}',
+    'libs/**/*/src/**/*.{js,jsx,ts,tsx}',
   ],
-  darkMode: 'media', // false or 'media' or 'class'
   theme: {
     extend: {},
   },
